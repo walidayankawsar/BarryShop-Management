5a95d6ab50ab850f4cbe47f54c6fad05716df96a
walidayankawsar <walidayankawsar@gmail.com>
2025-11-23 01:02:56 -0800

    new views created for new features

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 from django.contrib import messages
 from django.conf import settings
 
@@ -83,8 +84,18 @@ def contact(request):
 def about(request):
     return render(request, 'About.html')
 
+def profile(request):
+    return render(request, 'pages/profile.html')
+
 def priceing(request):
     return render(request, 'priceing.html')
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
 
 
 
@@ -123,3 +134,4 @@ def categories(request):
 @login_required
 def barcode(request):
     return render(request, 'pages/barcode_manager.html')
+
