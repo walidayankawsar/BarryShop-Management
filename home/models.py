d745ec1755e651b166d121999a195b4964e771b5
walidayankawsar <walidayankawsar@gmail.com>
2025-11-24 05:09:14 -0800

    models created for verify code

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class VerifiCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    used = models.BooleanField(default=False)
+
+    def is_expired(self):
+        from django.utils import timezone
+        from datetime import timedelta
+        return timezone.now() > self.created_at + timedelta(seconds=60)
