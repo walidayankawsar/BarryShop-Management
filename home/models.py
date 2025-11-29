e20e47828eef3cd8df7ecd20075c4898df606e23
walidayankawsar <walidayankawsar@gmail.com>
2025-11-29 03:17:57 -0800

    created product models

--- home/models.py
+++ home/models.py
@@ -13,3 +13,30 @@ class VerifiCode(models.Model):
         from django.utils import timezone
         from datetime import timedelta
         return timezone.now() > self.created_at + timedelta(seconds=60)
+    
+    class product(models.Model):
+
+
+        priority_list = [
+            ("low", "Low"),
+            ("medium", "Medium"),
+            ("high", "High"),
+        ]
+
+        category_list = [
+            ("electronics", "Electronics"),
+            ("groceries","Groceries"),
+            ("fitness","Fitness"),
+            ("unknown","Unknown"),
+        ]
+
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        barcode = models.IntegerField()
+        title = models.CharField(max_length=260)
+        price = models.DecimalField(max_digits=10, decimal_places=2)
+        sku = models.CharField(max_length=50, unique=True, help_text="Stock Keeping Unit (unique identifier)")
+        product_image = models.ImageField(upload_to='product_logos/', blank=True, null=True)
+        is_complited = models.BooleanField(default=False)
+        priority = models.CharField(max_length=10, choices=priority_list, default='medium')
+        category = models.CharField(max_length=20, choices=category_list, default='unknown')
+        created_at = models.DateTimeField(auto_now_add=True)
