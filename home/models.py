2682418e7646a6d7c6e9038ef887b05996704e43
walidayankawsar <walidayankawsar@gmail.com>
2025-11-29 05:44:08 -0800

    employee models created

--- home/models.py
+++ home/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+class Employee(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=10)
+    phone = models.IntegerField(max_length=15)
+    profile = models.ImageField(upload_to='profile_pic/', blank=True, null=True)
+    position = models.CharField(max_length=15)
+
 class VerifiCode(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=6)
@@ -18,9 +28,9 @@ class VerifiCode(models.Model):
 
 
         priority_list = [
-            ("low", "Low"),
-            ("medium", "Medium"),
-            ("high", "High"),
+            ("in stock", "In Stock"),
+            ("low stock", "Stock"),
+            ("out of stock", "Out of Stock"),
         ]
 
         category_list = [
@@ -28,6 +38,9 @@ class VerifiCode(models.Model):
             ("groceries","Groceries"),
             ("fitness","Fitness"),
             ("unknown","Unknown"),
+            ("stationery","Stationery"),
+            ("home & garden","Home & Garden"),
+            ("clothin", "Clothin"),
         ]
 
         user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -37,6 +50,6 @@ class VerifiCode(models.Model):
         sku = models.CharField(max_length=50, unique=True, help_text="Stock Keeping Unit (unique identifier)")
         product_image = models.ImageField(upload_to='product_logos/', blank=True, null=True)
         is_complited = models.BooleanField(default=False)
-        priority = models.CharField(max_length=10, choices=priority_list, default='medium')
+        priority = models.CharField(max_length=20, choices=priority_list, default='medium')
         category = models.CharField(max_length=20, choices=category_list, default='unknown')
         created_at = models.DateTimeField(auto_now_add=True)
