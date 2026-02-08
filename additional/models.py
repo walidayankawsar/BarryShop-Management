bf2f51bc335019220272023e4cded47169174445
walidayankawsar <walidayankawsar@gmail.com>
2026-02-08 07:06:42 -0800

    created models for messages

--- additional/models.py
+++ additional/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    msg = models.TextField(blank=True, null=True)
