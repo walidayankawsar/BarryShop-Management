76289681ad7b1edacaaad3613a24c4b20e770d32
walidayankawsar <walidayankawsar@gmail.com>
2026-02-08 06:45:45 -0800

    message views are added

--- additional/views.py
+++ additional/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import Msg_form
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
+
+def message(request):
+    if request.method == 'POST':
+        form = Msg_form(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your Messages sent Successfully')
+            return redirect('contact')
+    return render(request, 'contact.html')
