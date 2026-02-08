e06561d0d9ecfc4aa49e16e318b0c062ed984b2d
walidayankawsar <walidayankawsar@gmail.com>
2026-02-08 08:32:38 -0800

    email messages function added

--- additional/views.py
+++ additional/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import Msg_form
-from .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -9,7 +10,28 @@ def message(request):
     if request.method == 'POST':
         form = Msg_form(request.POST)
         if form.is_valid():
-            form.save()
+            mail = form.save()
             messages.success(request, 'Your Messages sent Successfully')
-            return redirect('contact')
+        
+            # now message will be sent in gmail
+            subject = "BarryShop Message"
+            message = f"""
+                Name: {mail.name}
+                Phone: {mail.phone}
+                Message: {mail.msg}
+            """
+
+            send_mail(
+                subject,
+                message,
+                settings.EMAIL_HOST_USER,
+                [settings.EMAIL_HOST_USER],
+                fail_silently=False,
+            )
+        else:
+            messages.error(request,'Your Messages fialed to send')
+    else:
+        form = Msg_form()
+        return redirect('contact')
+
     return render(request, 'contact.html')
